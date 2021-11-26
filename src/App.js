e4cc47cfa192897735dcdff67ac38856d20d9680
ogzhanolguncu <ogzhan11@gmail.com>
2021-11-27 00:43:59 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import dayjs from "dayjs";
+import "./app.css";
+
+const BASE_URL = "https://api.chucknorris.io/jokes";
+
+const fetchRandomJokeFromApi = async () => {
+  return axios.get(`${BASE_URL}/random`);
+};
 
 function App() {
+  const [joke, setJoke] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleFetchJoke = async () => {
+    const response = await fetchRandomJokeFromApi();
+    setJoke(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    handleFetchJoke();
+  }, []);
+
+  if (isLoading)
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+      >
+        Loading...
+      </div>
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <span>
+        Joke Creation Date: {dayjs(joke?.created_at).format("DD/MM/YYYY")}
+      </span>
+      <span>
+        Joke Update Date: {dayjs(joke?.updated_at).format("DD/MM/YYYY")}
+      </span>
+      <span>Joke: {joke?.value}</span>
+      <div>
+        <a href={joke?.url}>
+          <span>Would you like to visit joke's url?</span>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
